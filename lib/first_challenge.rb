cd35d19f98e22ac28ffb42c9c92da5242fb54ca5
Vijay Raju <vijaypraju04@gmail.com>
2017-09-18 06:24:53 +0000

    Manipulating Hashes

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +13,16 @@ def first_challenge
     }
   }
 
-  #your code here
-
+  contacts.each do |person, data|
+    data.each do |key, value|
+      if key == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+          flavor == "strawberry"
+        end
+      end
+    end
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
+end
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,8 +6,6 @@ def second_challenge
    grains: ["rice", "pasta"]
   }
 
-  #code your solution here!
-
-  
-
+  array = groceries.values
+  return array.flatten
 end
